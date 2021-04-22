9ab76ead8ef1adb8273376b6550ffb25efa1f34a
Rajat Dipta Biswas <rdbiswas97@gmail.com>
2021-04-22 14:18:53 +0600

    Encapsulate how the clock looks

--- src/index.js
+++ src/index.js
@@ -3,15 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 
-function tick() {
-  const element = (
+function Clock(props) {
+  return (
     <div>
       <h1>Hello, world!</h1>
-      <h2>It is {new Date().toLocaleTimeString()}.</h2>
+      <h2>It is {props.date.toLocaleTimeString()}.</h2>
     </div>
   );
+}
+
+function tick() {
   ReactDOM.render(
-    element,
+    <Clock date={new Date()}/>,
     document.getElementById('root')
   );
 }
