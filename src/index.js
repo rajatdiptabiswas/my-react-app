62448456a8dfcf186f44460ebf3c4339af464555
Rajat Dipta Biswas <rdbiswas97@gmail.com>
2021-04-27 22:43:22 +0600

    Basic list component

--- src/index.js
+++ src/index.js
@@ -2,14 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+function NumberList(props) {
+  const numbers = props.numbers;
+  const listItems = numbers.map((number) =>
+    <li key={number.toString()}>
+      {number}
+    </li>
+  );
+  return (
+    <ul>{listItems}</ul>
+  );
+}
+
 const numbers = [1, 2, 3, 4, 5];
-const listItems = numbers.map((number) =>
-  <li>{number}</li>
-);
-
-console.log(listItems);
-
 ReactDOM.render(
-  <ul>{listItems}</ul>,
+  <NumberList numbers={numbers} />,
   document.getElementById('root')
 );
